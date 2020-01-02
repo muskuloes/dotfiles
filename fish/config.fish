854de7dc7b93603b8bf3f00d341c6831254200ce
muskuloes <19honor@gmail.com>
2020-01-02 20:31:28 +0100

    add nnn variables to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
-set TERM xterm-256color
-set CDPATH . ~ ~/Desktop
+set -x TERM xterm-256color
+set -x CDPATH . ~ ~/Desktop
 
-set -gx PATH ~/bin /snap/bin /home/linuxbrew/.linuxbrew/bin ~/.local/bin ~/.kubectx ~/.fzf/bin $PATH
+set -gx PATH $HOME/bin /snap/bin /home/linuxbrew/.linuxbrew/bin $HOME/.local/bin $HOME/.kubectx $HOME/.fzf/bin $PATH
+set -x EDITOR nvim
 alias k="kubectl"
 alias f="fzf --preview 'bat --style=numbers --color=always {}'"
 alias vim="nvim"
@@ -9,3 +10,7 @@ alias r="R"
 
 eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
+set -x NNN_USE_EDITOR 1
+set -x NNN_CONTEXT_COLORS '4564'
+set -x NNN_BMS "u:$HOME/Desktop/lmu;D:$HOME/Downloads"
+set -x NNN_PLUG 'o:fzopen;p:mocplay;d:diffs;m:nmount;n:notes;v:imgviu;t:imgthumb'

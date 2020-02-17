6b24845bd1ff8c5cf786ebba6fd1103b77fb446a
muskuloes <19honor@gmail.com>
2020-02-17 20:35:23 +0100

    start tmux as fish function

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -x TERM xterm-256color
 set -x CDPATH . ~ ~/Desktop
 
-set -gx PATH $HOME/bin /snap/bin /home/linuxbrew/.linuxbrew/bin $HOME/.local/bin $HOME/.kubectx $HOME/.fzf/bin $PATH
+set -gx PATH /snap/bin /usr/local/go/bin $HOME/bin /home/linuxbrew/.linuxbrew/bin $HOME/.local/bin $HOME/.krew/bin $HOME/.kubectx $HOME/.fzf/bin $HOME/go/bin $PATH
 set -x EDITOR nvim
 alias k="kubectl"
 alias f="fzf --preview 'bat --style=numbers --color=always {}'"
@@ -14,3 +14,4 @@ set -x NNN_USE_EDITOR 1
 set -x NNN_CONTEXT_COLORS '4564'
 set -x NNN_BMS "u:$HOME/Desktop/lmu;D:$HOME/Downloads"
 set -x NNN_PLUG 'o:fzopen;p:mocplay;d:diffs;m:nmount;n:notes;v:imgviu;t:imgthumb'
+start_tmux

6d0a01bd097794257bf5cb878c4b78799bf92ee5
muskuloes <t.victor@campus.lmu.de>
2020-12-23 20:55:34 +0100

    add alias for ls using exa

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -x TERM xterm-256color
 set -x CDPATH . ~ ~/Desktop
 
-set -gx PATH /snap/bin /usr/local/go/bin $HOME/bin /home/linuxbrew/.linuxbrew/bin $HOME/.local/bin $HOME/.krew/bin $HOME/.kubectx $HOME/.fzf/bin $HOME/go/bin $HOME/.yarn/bin $PATH
+set -gx PATH /snap/bin /usr/local/go/bin $HOME/bin /home/linuxbrew/.linuxbrew/bin $HOME/.local/bin $HOME/.krew/bin $HOME/.kubectx $HOME/go/bin $HOME/.yarn/bin $PATH
 set -gx EDITOR vim
 set -gx VISUAL ewrap
 set -gx NNN_BMS 'u:~/Desktop/uni;w:~/Desktop/work'
@@ -13,9 +13,11 @@ alias vim="nvim"
 alias n="nnn -exC"
 alias k="kubectl"
 alias r="R"
+alias ls="exa --long --header --git --group"
+
+set -g fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/muskuloes/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
